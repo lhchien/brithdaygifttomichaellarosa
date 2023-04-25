345e4275144860b7e22561da62370fb24887cb11
Le Hong Chien <37591890+lhchien@users.noreply.github.com>
2023-04-25 23:58:38 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Michael La Rosa';
+const giftUrl = '';
+const eventName = 'Happy Brithday Darling';
+const titleCard = 'Michael La Rosa';
+const contentCard = "Happy Birthday! Even though I'm not there today, be excited for the gifts that are coming your way, as the best is yet to come, so look forward to that my handsome one.";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
